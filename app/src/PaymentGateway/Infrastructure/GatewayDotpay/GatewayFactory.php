582b2d0c3b8c6cbdc65ef1872ff2caf4ed837419
Athlan <athlan@vgroup.pl>
2019-05-03 09:48:47 +0200

    Payment process scaffold

---
+++ app/src/PaymentGateway/Infrastructure/GatewayDotpay/GatewayFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+
+namespace App\PaymentGateway\Infrastructure\GatewayDotpay;
+
+
+use App\PaymentGateway\Domain\Model\GatewayFactory as IGatewayFactory;
+use App\PaymentGateway\Domain\Model\Payment;
+use Omnipay\Common\GatewayInterface;
+use Omnipay\Omnipay;
+
+class GatewayFactory implements IGatewayFactory
+{
+    /**
+     * @var array
+     */
+    private $params;
+
+    /**
+     * GatewayFactory constructor.
+     * @param array $params
+     */
+    public function __construct(array $params)
+    {
+        $this->params = $params;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function createGateway(): GatewayInterface
+    {
+        $gateway = Omnipay::create('Dotpay');
+
+        $gateway->initialize($this->params);
+
+        return $gateway;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    function createPurchaseParams(Payment $payment): array
+    {
+        return [
+            'control' => $payment->getPaymentId(),
+        ];
+    }
+}
